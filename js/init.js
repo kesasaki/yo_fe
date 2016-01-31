55ceddb57efd9a8ddb53c50c643c39d96e8e42f8
norisu0313 <norisu0313@gmail.com>
2016-01-31 16:52:15 +0900

    曲リストを itunes から取得する

--- js/init.js
+++ js/init.js
@@ -7,7 +7,7 @@
 //  }); // end of document ready
 //})(jQuery); // end of jQuery name space
 
-app = angular.module('yo_fe', []);
+app = angular.module('yo_fe', ['yoServices']);
 app.controller('smallMenuController', ['$scope', function($scope) {
   $scope.onclick = function() {
     t = $('.toggle-menu')
@@ -58,4 +58,44 @@ app.controller('newsToggleController', ['$scope', function($scope) {
     body.toggle();
     target.toggleClass("toggle");
   });
+}]);
+
+angular.module('yoServices', []).service('itunes', ['$http', function ($http) {
+  this.get_album = function (query, callback) {
+    var url = 'https://itunes.apple.com/lookup?callback=JSON_CALLBACK&entity=song&country=JP&id=' + query;
+    return $http.jsonp(url).then(
+      function (response) {
+        albumInfo = {};
+        songs = [];
+        $.each(response.data.results, function(index, object){
+          if (object.collectionType !== undefined && object.collectionType === "Album") {
+            albumInfo = object;
+          }
+          if (object.kind !== undefined && object.kind === "song") {
+            songs.push(object);
+          }
+        });
+        songs.sort(function(a, b){
+          if(a.trackNumber < b.trackNumber) return -1;
+          if(a.trackNumber > b.trackNumber) return 1;
+          return 0;
+        });
+        return {info: albumInfo, songs: songs};
+      },
+      function (response) {
+        alert(response.data.message);
+        return {info: {}, songs: []};
+      }
+    );
+  }
+}]);
+
+app.controller('itunesController', ['$scope', 'itunes', '$sce', '$element', function ($scope, itunes, $sce, $element) {
+  $scope.trustSrc = function(src) {
+    return $sce.trustAsResourceUrl(src);
+  };
+  itunes.get_album($element.attr("data-itunes-id")).then(function(res){
+    console.log(res);
+    $scope.album = res;
+  })
 }]);
